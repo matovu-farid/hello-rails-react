2d02ff2799544dcceec2f763bc71c1ff3fbe8446
matovu-farid <matovu90@gmail.com>
2022-05-30 16:58:27 +0300

    Generate Message

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,53 @@
+class MessagesController < ApplicationController
+  before_action :set_message, only: %i[ show update destroy ]
+
+  # GET /messages
+  # GET /messages.json
+  def index
+    @messages = Message.all
+  end
+
+  # GET /messages/1
+  # GET /messages/1.json
+  def show
+  end
+
+  # POST /messages
+  # POST /messages.json
+  def create
+    @message = Message.new(message_params)
+
+    if @message.save
+      render :show, status: :created, location: @message
+    else
+      render json: @message.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /messages/1
+  # PATCH/PUT /messages/1.json
+  def update
+    if @message.update(message_params)
+      render :show, status: :ok, location: @message
+    else
+      render json: @message.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /messages/1
+  # DELETE /messages/1.json
+  def destroy
+    @message.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_message
+      @message = Message.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def message_params
+      params.require(:message).permit(:text)
+    end
+end
