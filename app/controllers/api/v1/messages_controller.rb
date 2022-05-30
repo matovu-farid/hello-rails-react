76a1e84fdf000e61838bb038919ff507d71b397f
matovu-farid <matovu90@gmail.com>
2022-05-30 18:59:33 +0300

    Add linters

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,9 +1,6 @@
 class Api::V1::MessagesController < ApplicationController
- 
   def random
     message = Message.find(Message.pluck(:id).sample)
     render json: message, status: :ok
-
   end
-  
 end
